495db20ff0755100aeb2739219948c8382276814
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2020-11-20 20:08:58 +0100

    second commit

--- bot.py
+++ bot.py
@@ -2,19 +2,38 @@ from telegram.ext import Updater, CommandHandler
 import requests
 import re
 
+def get_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
 def get_url():
     contents = requests.get('https://random.dog/woof.json').json()    
     return contents["url"]
 
-def bop(bot, update):
-    url = get_url()
+def greet(bot, update):
+    text = """
+    Welcome To Tee-py Telegram Bot 👋.
+    I am Here to send you random Dog Images from the internet.👍
+
+    enter: /dog to get a random Dog Image from the internet.
+    """
+    update.message.reply_text(text=text)
+
+def dog(bot, update):
+    url = get_image_url()
     chat_id = update.message.chat_id
+    print(chat_id)
     bot.send_photo(chat_id=chat_id, photo=url)
 
 def main():
-    updater = Updater('1432662407:AAGqtsCjDmepId-U5PiZOkjvspLCcmGkGrM')
+    updater = Updater('1432662407:AAGqtsCjDmepId-U5PiZOkjvspLCcmGkGrM', use_context=False)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('bop',bop))
+    dp.add_handler(CommandHandler('dog', dog))
+    dp.add_handler(CommandHandler('start', greet))
     updater.start_polling()
     updater.idle()
     
