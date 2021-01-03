ece40c3e272d7eba33f4b3b4b763e3a6b6b87a4a
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2021-01-03 22:01:21 +0100

    implemented regex updates

--- bot3.py
+++ bot3.py
@@ -19,37 +19,37 @@ async def broadcast(event):
     #Get the Signal
     message = event.original_update.message
     try:
+        #Trying to know if message is coming from a channel
         c_id = message.peer_id.channel_id
         print(c_id)
         message = message.message
     except:
         pass
+    #Get All Subscribers
     all_subscribers = fauna_client.query(
         q.paginate(q.documents(q.collection('users')))
     )
+    #Start sending messages
     for subscriber in all_subscribers['data']:
         user = fauna_client.query(q.get(q.ref(q.collection("users"), subscriber.__dict__["value"]["id"])))
         text = f"NEW SIGNAL UPDATE 💰📶🔊📣🚀‼️‼️\n\n{message}"
         await client.send_message(user["data"]["chat_id"], text)  
 
-@client.on(events.NewMessage(pattern="start"))
+@client.on(events.NewMessage(pattern=re.compile(r"start", re.I)))
 async def start(event):
     message = event.message
-    #print(event)
     try:
         from_id = message.from_id.user_id
         sent_by_id = message.peer_id.user_id
         async with client.conversation(sent_by_id) as conv:
             prompt = await conv.send_message(f"Welcome 👋. Please Enter passcode 🔑 to start receiving signal updates.")
             code = await conv.get_response()
-            #print(code)
             if code.message == "12345":
                 try:
-                    fauna_client.query(q.get(q.match(q.index("users"), sent_by_id)))
+                    fauna_client.query(q.get(q.match(q.index("chat_id"), sent_by_id)))
                     await conv.send_message(f"""
                     Welcome, 👋 🎉.\nYour Details exists in our DataBase 🥳.\nYou can now start recieving signal updates.""")
                 except Exception as e:
-                    #print(e)
                     try:
                         user = fauna_client.query(q.create(q.collection("users"), {
                             "data": {
@@ -60,14 +60,12 @@ async def start(event):
                         await conv.send_message(f"""
                         Welcome 👋 🎉 🎉.\nYour Details have now been stored in our database 🥳.\nYou are now able to receive Forex Signals sent to the Signal Group 🚀🚀.""")
                     except Exception as e:
-                        #print(e) 
                         await conv.send_message('An Error Occurred while saving your details😞😭. Please Try Again.')            
             else:
                 await conv.send_message(f"Invalid Passcode ❌. Contact Admin For Passcode.")
 
     except Exception as e:
-        print(e)
-        print("This is not a user!!")
+        await conv.send_message('An Error Occurred while saving your details😞😭. Please Try Again.')    
     #async with client.conversation(event.)
 
 with client:
